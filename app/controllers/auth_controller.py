4d2a8c1b18ff5722846653a4363ed5bf09f5d5a9
kazuki <kazukishiroma06@gmail.com>
2026-05-05 13:03:45 -0300

    auth controller iniciado

---
+++ app/controllers/auth_controller.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends, Request, Form, status
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app.models.usuario import Usuario
+from app.auth import hash_senha, verificar_senha, criar_token
+
+router = APIRouter(prefix="/auth", tags=["Autenticação"])
+
+templates = Jinja2Templates(directory="app/templates")
+
+#Rota de cadastro
+@router.get("/cadastro")
+def tela_cadastro(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "auth/cadastro.html",
+        {"request": request}
+    )
--- app/main.py
+++ app/main.py
@@ -3,7 +3,14 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 
+from app.controllers import auth_controller
+
 app = FastAPI(title="Sistema estoque")
 
 #Configurar o Fastapi para servir os arquivos estáticos (CSS, JV, IMGS)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+#Inclui os routers dos controles
+app.include_router(auth_controller.router)
